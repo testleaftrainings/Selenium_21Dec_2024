df6e63e9c0a600c46d43281ca01dbae27a47c14a
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2025-01-19 20:01:34 +0530

    Add files via upload

---
+++ Week5/Day2/BaseClass.java
@@ -0,0 +1,39 @@
+package testcases;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+
+public class BaseClass {
+	
+	public ChromeDriver driver;
+	
+	@Parameters({"URL","Username","Password"})
+	@BeforeMethod
+	public void preCondition(String url,String user, String pass) {
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get(url);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.findElement(By.id("username")).sendKeys(user);
+		driver.findElement(By.id("password")).sendKeys(pass);
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+
+
+	}
+	@AfterMethod
+	public void postCondition() {
+		driver.close();
+
+	}
+	
+	
+	
+
+}
